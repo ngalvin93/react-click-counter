317157b85705c0112f67d78fd4427b5d4a882269
Alvin Ng <ngalvin93@gmail.com>
2020-03-11 23:12:24 -0500

    style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Clicker from './components/Clicker';
 import { ThemeProvider } from 'emotion-theming';
+import { Flex, Box } from 'rebass'
 import theme from './theme'
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Clicker />
-      </div>
+      <Flex className="App">
+        <Box>
+          <Clicker />
+        </Box>
+      </Flex>
     </ThemeProvider>
   );
 }
